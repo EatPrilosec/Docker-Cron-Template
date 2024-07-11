3c79f01de44ee659bbaf59375de6438dd40f9ccb
EatPrilosec <ae3nerdgod@gmail.com>
2024-07-10 21:28:48 -0400

    Update cron_start.sh

--- files/cron_start.sh
+++ files/cron_start.sh
@@ -16,7 +16,12 @@ CronCommand="/usr/bin/env"
 
 ################################# do not edit if confused ########################################
 env >/opt/env
-useradd --no-create-home --no-log-init -Uu 1000 --password $RANDOM dockercronjobworker
+if [ "$PUID" != "$PGID" ]; then
+  groupadd -g $PGID
+  useradd --no-create-home --no-log-init -Uu $PUID --password $RANDOM dockercronjobworker
+else
+  useradd --no-create-home --no-log-init -Uu $PUID --password $RANDOM dockercronjobworker
+fi
 echo "$CronSchedule sudo -E --group=dockercronjobworker --user=dockercronjobworker $CronCommand >/opt/cron.log 2>/opt/cron.log" >/opt/cron
 crontab /opt/cron
 touch /opt/cron.log
